05925b371e603796dc6cc6a1faa4ec3c5d47cc7d
an2003na <127742388+an2003na@users.noreply.github.com>
2023-03-23 19:02:30 +0400

    Add files via upload

--- different_languages/tcl/linear_equation/test/test.tcl
+++ different_languages/tcl/linear_equation/test/test.tcl
@@ -1,1 +1,20 @@
+#!/usr/bin/env tclsh
 
+
+source ../src/linear_equation.tcl
+
+proc main {} {
+    set k 2
+    set b 2
+    set x 2
+    set y 2
+    if {[find_x 2 2 2] == 0 && [find_y 2 2 2] == 6} {
+        puts "Test passed."
+    } else {
+        puts "Test failed."
+    }
+}
+
+if { $::argv0 eq [info script] } {
+    main
+}
