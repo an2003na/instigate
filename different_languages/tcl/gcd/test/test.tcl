8d5dc51f6ce139aa03b38d9474d9c40c16ee04ce
an2003na <127742388+an2003na@users.noreply.github.com>
2023-03-23 18:50:37 +0400

    Add files via upload

--- different_languages/tcl/gcd/test/test.tcl
+++ different_languages/tcl/gcd/test/test.tcl
@@ -1,1 +1,49 @@
+#!/usr/bin/env tclsh
 
+
+source ../src/gcd.tcl
+
+proc test1 {} {
+    set a 25
+    set b 625
+    set res [gcd $a $b ]
+    if {$res == 25} {
+        puts "Test 1 passed."
+    } else {
+        puts "Test 1 failed."
+    }
+}
+
+
+proc test2 {} {
+    set a 13
+    set b 92
+    set res [gcd $a $b ]
+    if {$res == 1} {
+        puts "Test 2 passed."
+    } else {
+        puts "Test 2 failed."
+    }
+}
+
+
+proc test3 {} {
+    set a 64
+    set b 12
+    set res [gcd $a $b ]
+    if {$res == 4} {
+        puts "Test 3 passed."
+    } else {
+        puts "Test 3 failed."
+    }
+}
+
+proc main {} {
+    test1
+    test2
+    test3
+}
+
+if { $::argv0 eq [info script] } {
+    main
+}
