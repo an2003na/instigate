b65b2e1067875b51e8cf3d0f1429c28284218c20
an2003na <127742388+an2003na@users.noreply.github.com>
2023-03-27 17:59:18 +0400

    Add files via upload

--- different_languages/julia/gcd/src/main.jl
+++ different_languages/julia/gcd/src/main.jl
@@ -1,1 +1,19 @@
+#!/usr/bin/env julia
+
+
+include("gcd.jl")
+
+#the main() function runs functions from gcd.jl file
+function main()
+    a = check_validation("Enter a positive integer for a: ")
+    b = check_validation("Enter a positive integer for b: ")
+    result = gcd(a, b)
+    println("The GCD of $a and $b is: $result")
+end
+
+
+if (abspath(PROGRAM_FILE) == @__FILE__)
+    main()
+end
+
 
